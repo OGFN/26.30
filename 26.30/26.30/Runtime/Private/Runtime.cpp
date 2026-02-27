f1ca4697e1cc9a94aa91229cd38f03f4feccf885
Aaron <tezzy3316@gmail.com>
2026-02-27 14:37:06 -0600

    push

--- 26.30/26.30/Runtime/Private/Runtime.cpp
+++ 26.30/26.30/Runtime/Private/Runtime.cpp
@@ -6,40 +6,10 @@ void Runtime::LoadConsole()
 	FILE* File;
 	freopen_s(&File, "CONOUT$", "w", stdout);
 	freopen_s(&File, "CONOUT$", "w", stderr);
-	SetConsoleTitleA("FGS - 26.30 || Starting...");
+	SetConsoleTitleA("26.30 || Starting...");
 }
 
 void Runtime::LOG(const string& msg)
 {
-	cout << "[*] FGS: 26.30 >> " << msg << endl;
+	cout << "[*] 26.30 >> " << msg << endl;
 }
-
-void Runtime::Hook(uint64_t Address, PVOID Hook, void** Original)
-{
-	MH_CreateHook(LPVOID(Address), Hook, Original);
-	MH_EnableHook(LPVOID(Address));
-}
-
-void Runtime::VHook(void* Base, int Idx, void* Detour, void** Original)
-{
-	DWORD oldProt;
-	void** VTable = *(void***)Base;
-	if (Original)
-		*Original = VTable[Idx];
-
-	VirtualProtect(&VTable[Idx], sizeof(void*), PAGE_EXECUTE_READWRITE, &oldProt);
-	VTable[Idx] = Detour;
-	VirtualProtect(&VTable[Idx], sizeof(void*), oldProt, &oldProt);
-}
-
-int Runtime::ReturnTrue()
-{
-	return 1;
-}
-
-int Runtime::ReturnFalse()
-{
-	return 0;
-}
-
-void Runtime::NullFunc() {}
--- 26.30/26.30/Runtime/Public/Runtime.h
+++ 26.30/26.30/Runtime/Public/Runtime.h
@@ -7,9 +7,34 @@ class Runtime
 public:
 	static void LoadConsole();
 	static void LOG(const string& msg);
-	static void Hook(uint64_t Address, PVOID Hook, void** Original = nullptr);
-	static void VHook(void* Base, int Idx, void* Detour, void** Original = nullptr);
-	static int ReturnTrue();
-	static int ReturnFalse();
-	static void NullFunc();
+
+	static void Hook(uint64_t Address, PVOID Hook, void** Original = nullptr)
+	{
+		MH_CreateHook(LPVOID(Address), Hook, Original);
+		MH_EnableHook(LPVOID(Address));
+	}
+
+	static void VHook(void* Base, int Idx, void* Detour, void** Original = nullptr)
+	{
+		DWORD oldProt;
+		void** VTable = *(void***)Base;
+		if (Original)
+			*Original = VTable[Idx];
+
+		VirtualProtect(&VTable[Idx], sizeof(void*), PAGE_EXECUTE_READWRITE, &oldProt);
+		VTable[Idx] = Detour;
+		VirtualProtect(&VTable[Idx], sizeof(void*), oldProt, &oldProt);
+	}
+
+	static int ReturnTrue()
+	{
+		return 1;
+	}
+
+	static int ReturnFalse()
+	{
+		return 0;
+	}
+
+	static void NullFunc() {}
 };
